23204b786bd4c6399089122834863357080ef1f3
durban <zhangdapeng89@126.com>
2015-07-25 13:56:38 +0800

    修改倍数范围可以自定义

--- bin/main.js
+++ bin/main.js
@@ -7,15 +7,19 @@ var colors = require('colors');
 var util = require('util');
 var program = require('commander');
 
+function list(val) {
+  return val.split(',');
+}
 
 //定义参数,以及参数内容的描述  
 program
     .version('0.0.1')  
     .usage('[options] [value ...]')
+    .option('-l, --list <items>','倍数数组',list,[1,3,8,24,50,100]) 
     .option('-b, --base <n>','下注的个数',parseInt,5) 
     .option('-x, --max <n>', '下注的一注的最大价格',parseFloat,5)  
     .option('-n, --min <n>', '下注的一注的最小价格',parseFloat,0.1)  
-    .option('-ml, --max_length <n>', '输出字符的最大长度数', parseInt, 70);
+    .option('-ml, --length <n>', '输出字符的最大长度数', parseInt, 100);
 
 program.on('--help', function(){
   console.log('  例如:');
@@ -24,33 +28,30 @@ program.on('--help', function(){
   console.log('    $ caipiao -h');
   console.log('    $ caipiao -x 3.0');
   console.log('    $ caipiao -n 0.2');
+  console.log('    $ caipiao --length 1,3,8,24,50,100');
   console.log('');
 });
 
 program.parse(process.argv);
 
-var level = [
-  1,3,8,24,50,100
-];
-
+var level = program.list;
 var base = program.base;
-
 var max = program.max;
 var min = program.min;
-
-var max_length = program.max_length;
-
+var max_length = program.length;
 for(var i = min;i < max;i += 0.1){
   var a = parseFloat(i).toFixed(1);
   console.log(util.format('基数为：%s'.green,a));
 
   //star line
+  
   var star_line = _.fill(Array(max_length), '*');
   star_line = star_line.join('');
   console.log(star_line.yellow);
 
   //per String
   var a_level = _.map(level,function(b){
+    b = parseFloat(b);
     var tmp = b * a;
     return parseFloat(tmp).toFixed(2);
   });
